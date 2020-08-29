bf939198bb3e05fa9a9b4e38fa2c824a8d7026f6
Disha <disha@dishanagpure2.com>
2020-08-29 14:55:40 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+#print("Hello world")
+#print(3+5)
+#print("How are you?")
+#print("Shut up!")
+#import cv2
+#import math
+#print(math.gcd(12,69))
+a=8
+b=6
+c=2
+print(a+b+c)
+print(a*c)
+print(b/c)
+print(b-c-a)
+print(c+3)
+print(c)
+print(8)
+print(type(c))
+print("Disha")
+d=3.14
+print(type(d))
+e="12"
+e=int(12)
+e=float(e)
+print(e-2)
+#f=Disha
+#print(type(f))
+#e=float(e)
+f=11
+f=str(f)
+f=float(f)
+print(f+1)
+g=1.0
+#g=str(g)
+#g=int(g)
+print(g+1)
+name="Disha"
+print(name[0])
+print(name[0:5])
+print(name[1:4])
+print(name[1:5])
+print(len(name))
+var=name.lower()
+var=name.upper()
+print(var)
