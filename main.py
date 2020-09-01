412dd23ec72a76bd57bf85023df0b037c9c5127c
Disha <disha@dishanagpure2.com>
2020-09-01 17:10:06 +0530

    1/9/2020

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-#print("Hello world")
-#print(3+5)
-#print("How are you?")
-#print("Shut up!")
-#import cv2
-#import math
-#print(math.gcd(12,69))
+print("Hello world")
+print(3+5)
+print("How are you?")
+print("Shut up!")
+import cv2
+import math
+print(math.gcd(12,69))
 a=8
 b=6
 c=2
@@ -46,3 +46,18 @@ print(var)
 print("My name is Disha")
 print(85.2+56)
 print("Hi")
+var=name.replace("i","e")
+print(var)
+var=name.replace("e","i")
+print(var)
+str1="A friend in need "
+str2="is a friend indeed"
+print(str1+str2)
+name1="Disha"
+name2="Deepali"
+temp=f"{name1} is very intelligent as compared to {name2}."
+print(temp)
+yo = [61,22,5,8,9,62]
+var = type(yo)
+var = yo[0:6]
+print(var)
